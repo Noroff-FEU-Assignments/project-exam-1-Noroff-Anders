a83f4a691ba1dd631c387db0a673e288e71e0bdb
Noroff-Anders <AndNes16878@stud.noroff.no>
2023-05-03 22:19:44 +0200

    starting on home page

--- JS/blog-card.js
+++ JS/blog-card.js
@@ -1,14 +1,39 @@
-const wpUrl = 'https://project-exam.wp-andersnes.no/wp-json/wp/v2/'
+const wpUrl = 'https://project-exam.wp-andersnes.no/wp-json/wp/v2/posts?_embed'
 
 
 async function getPosts() {
     try {
-        const response = await fetch(wpUrl + 'posts?_embed')
+        const response = await fetch(wpUrl)
         const posts = await response.json()
-        console.log(posts)
+        return posts
     } catch (error) {
         console.log(error)
     }
 }
 
-getPosts();
+async function renderblogcard() {
+    const post = await getPosts();
+        post.forEach((blogpost) => {
+
+            const title = blogpost.title.rendered;
+            const featuredImageURL = blogpost._embedded['wp:featuredmedia']['0'].source_url;
+        
+            // Create a new element for the title
+            const titleElement = document.createElement('h1');
+            titleElement.textContent = title;
+            titleElement.classList.add('blog-card-title');
+        
+            // Create a new element for the featured image
+            const imageElement = document.createElement('img');
+            imageElement.src = featuredImageURL;
+            imageElement.classList.add('blog-card-image');
+        
+            // Append the title and image elements to a parent element
+            const parentElement = document.getElementById('div-last-post');
+            parentElement.appendChild(titleElement);
+            parentElement.appendChild(imageElement);
+         });
+
+}
+
+renderblogcard();
