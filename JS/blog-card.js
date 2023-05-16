94b7d15a7df4c560f020f7d7237e764edac1e943
Noroff-Anders <AndNes16878@stud.noroff.no>
2023-05-16 13:15:21 +0200

    fixing the carousel

--- JS/blog-card.js
+++ JS/blog-card.js
@@ -1,10 +1,11 @@
 const wpBaseUrl = 'https://project-exam.wp-andersnes.no/wp-json/wp/v2/posts';
-const perPage = 10;
+const perPage = 4; // Number of posts to fetch per page
 const parentElement = document.getElementById('div-last-post');
+let currentPage = 1; // Track the current page
 
 async function getPosts(page) {
-    const url = `${wpBaseUrl}?_embed&per_page=${perPage}&page=${page}`;
-    try {
+  const url = `${wpBaseUrl}?_embed&per_page=${perPage}&page=${page}`;
+  try {
     const response = await fetch(url);
     const posts = await response.json();
     return posts;
@@ -14,27 +15,12 @@ async function getPosts(page) {
 }
 
 async function renderBlogCards() {
-  let page = 1;
-  let allPosts = [];
+  const posts = await getPosts(currentPage);
 
-  while (true) {
-    const posts = await getPosts(page);
+  // Clear the parent element
+  parentElement.innerHTML = '';
 
-    if (posts.length === 0) {
-      // No more posts, exit the loop
-      break;
-    }
-
-    allPosts = allPosts.concat(posts);
-    page++;
-
-    if (page > 12) {
-      // Maximum limit reached (optional)
-      break;
-    }
-  }
-
-  allPosts.forEach((blogpost) => {
+  posts.forEach((blogpost) => {
     const blogcard = document.createElement('div');
     blogcard.classList.add('blog-card');
 
@@ -58,4 +44,31 @@ async function renderBlogCards() {
   });
 }
 
+// Function to handle the next button click event
+function handleNextButtonClick() {
+  currentPage++; // Increment the current page
+
+  if (currentPage > 3) {
+    currentPage = 1; // Go back to the starting page
+  }
+  renderBlogCards(); // Render the next set of posts
+}
+
+function handleBackButtonClick() {
+  currentPage--; // Increment the current page
+  if (currentPage < 1) {
+    currentPage = 3; // Go back to the starting page
+  }
+
+  renderBlogCards(); // Render the next set of posts
+}
+
+// Attach the next button click event listener
+const nextButton = document.getElementById('btn-right');
+nextButton.addEventListener('click', handleNextButtonClick);
+
+const backButton = document.getElementById('btn-left');
+backButton.addEventListener('click', handleBackButtonClick);
+
+// Initial rendering of the blog cards
 renderBlogCards();
