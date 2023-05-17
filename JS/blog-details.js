171ff50d706b1e10470c96781f60a7303d3703a1
Noroff-Anders <AndNes16878@stud.noroff.no>
2023-05-17 21:18:09 +0200

    fisxing blog details page

--- JS/blog-details.js
+++ JS/blog-details.js
@@ -5,7 +5,6 @@ const postUrl = `${wpBaseUrl}/${postId}`;
 
 const parentElement = document.getElementById('container-blog-detail');
 
-
 async function getPost() {
   try {
     const response = await fetch(postUrl);
@@ -17,19 +16,50 @@ async function getPost() {
 }
 
 async function renderBlogPost() {
-    const post = await getPost();
-  
-    // Render the blog post details on the page
-    const title = document.createElement('h1');
-    title.textContent = post.title.rendered.textContent; // Assign the title text
-  
-   // Render the blog post details on the page
-   const content = document.createElement('p');
-   content.textContent = post.content.rendered; // Assign the title text
-    // Render the title and content elements on the page as desired
-    // ...
-    parentElement.appendChild(title);
-    parentElement.appendChild(content);
-  }
+  const post = await getPost();
+
+  // Render the blog post details on the page
+  const title = document.createElement('h1');
+  title.textContent = post.title.rendered; // Assign the title text
+
+  // Render the blog post details on the page
+  const content = document.createElement('div');
+  content.innerHTML = post.content.rendered; // Assign the content as HTML
+
+  // Wrap images with a container and add classes
+  const images = content.querySelectorAll('img');
+  images.forEach((image) => {
+    const container = document.createElement('div');
+    container.classList.add('image-container');
+    image.parentNode.insertBefore(container, image);
+    container.appendChild(image);
+
+    // Add click event listener to each image
+    image.addEventListener('click', (event) => {
+      event.stopPropagation(); // Stop event propagation to prevent closing the popup
+
+      // Hide other open popups
+      const openPopups = document.querySelectorAll('.image-container.popup');
+      openPopups.forEach((popup) => {
+        popup.classList.remove('popup');
+      });
+
+      container.classList.add('popup');
+    });
+  });
+
+  // Add click event listener to the document to close the popup when clicked outside the image or overlay
+  document.addEventListener('click', (event) => {
+    const openPopups = document.querySelectorAll('.image-container.popup');
+    openPopups.forEach((popup) => {
+      if (!popup.contains(event.target) && !event.target.classList.contains('overlay')) {
+        popup.classList.remove('popup');
+      }
+    });
+  });
+
+  parentElement.appendChild(title);
+  parentElement.appendChild(content);
+}
 
 renderBlogPost();
