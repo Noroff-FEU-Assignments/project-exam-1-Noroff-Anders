de318738020ddee70d8c245b22522f116cd4f014
Noroff-Anders <AndNes16878@stud.noroff.no>
2023-05-18 22:54:15 +0200

    fixing contact form page validation

--- JS/all-blog-cards.js
+++ JS/all-blog-cards.js
@@ -1,9 +1,10 @@
 const wpBaseUrl = 'https://project-exam.wp-andersnes.no/wp-json/wp/v2/posts';
 const parentElement = document.getElementById('div-last-post');
-let currentPage = 1; 
+const loadMoreButton = document.getElementById('load-more-button');
+let currentPage = 1;
 
 async function getPosts(page) {
-  const url = `${wpBaseUrl}?_embed&per_page=${perPage}&page=${page}`;
+  const url = `${wpBaseUrl}?_embed&page=${page}`;
   try {
     const response = await fetch(url);
     const posts = await response.json();
@@ -16,34 +17,46 @@ async function getPosts(page) {
 async function renderBlogCards() {
   const posts = await getPosts(currentPage);
 
-  // Clearing the parent element
-  parentElement.innerHTML = '';
-
   posts.forEach((blogpost) => {
-    const blogcard = document.createElement('div');
-    blogcard.classList.add('blog-card');
-
-    const title = blogpost.title.rendered;
-    const featuredImageURL = blogpost._embedded['wp:featuredmedia'][0].source_url;
-
-    const titleElement = document.createElement('h1');
-    titleElement.textContent = title;
-    titleElement.classList.add('blog-card-title');
-
-
-    const imageElement = document.createElement('img');
-    imageElement.src = featuredImageURL;
-    imageElement.classList.add('blog-card-image');
-
-    blogcard.appendChild(imageElement);
-    blogcard.appendChild(titleElement);
-    parentElement.appendChild(blogcard);
-
-    blogcard.addEventListener('click', () => {
-      window.location.href = `blogdetail.html?id=${blogpost.id}`;
-    });
+      const blogcard = document.createElement('div');
+      blogcard.classList.add('blog-card');
+  
+      const title = blogpost.title.rendered;
+      const featuredImageURL = blogpost._embedded['wp:featuredmedia'][0].source_url;
+  
+      const titleElement = document.createElement('h1');
+      titleElement.textContent = title;
+      titleElement.classList.add('blog-card-title');
+  
+  
+      const imageElement = document.createElement('img');
+      imageElement.src = featuredImageURL;
+      imageElement.classList.add('blog-card-image');
+  
+      blogcard.appendChild(imageElement);
+      blogcard.appendChild(titleElement);
+      parentElement.appendChild(blogcard);
+  
+      blogcard.addEventListener('click', () => {
+        window.location.href = `blogdetail.html?id=${blogpost.id}`;
+      });
   });
+
+  // Enable the load more button if there are more posts
+  if (posts.length >= 10) {
+    loadMoreButton.disabled = false;
+    
+  } else {
+    loadMoreButton.disabled = true;
+    loadMoreButton.style.display = 'none';
+  }
 }
 
+async function loadMorePosts() {
+  currentPage++;
+  await renderBlogCards();
+}
+
+loadMoreButton.addEventListener('click', loadMorePosts);
 
 renderBlogCards();
