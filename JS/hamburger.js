238fb9b7ce97937ce556f54296679312112265ac
Noroff-Anders <AndNes16878@stud.noroff.no>
2023-05-21 21:35:15 +0200

    fising styling and finishing up pages, compressed images

---
+++ JS/hamburger.js
@@ -0,0 +1,18 @@
+const hamburgerMenu = document.querySelector(".div-hamburger-icon");
+const nav = document.querySelector(".nav-button");
+
+hamburgerMenu.addEventListener("click", () => {
+  if (nav.style.display === "none") {
+    nav.style.display = "flex";
+  } else {
+    nav.style.display = "none";
+  }
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 1300) {
+    nav.style.display = "flex";
+  } else {
+    nav.style.display = "none";
+  }
+});
